1e1db1cf1859e553f241a2de93241ce9341f64fe
healthy-restaurant <chivate.rsr@gmail.com>
2025-02-06 21:16:04 -0500

    Initial commit

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -7,9 +7,10 @@ export default function Resources() {
     return (
         <div className='container'>
 
-            <img className='info-images' src='../images/webmaster-checkright.png' alt="copyright-checklist" />
-            <img className='info-images' src='../images/webmaster-work.png' alt="copyright-checklist" />
+            <img className='info-images' src='https://healthyresta.github.io/dinner/images/webmaster-checkright.png' alt="copyright-checklist" />
+            <img className='info-images' src='https://healthyresta.github.io/dinner/images/webmaster-work.png' alt="copyright-checklist" />
             <div><h1>All the images and graphics found on this website are created through Canva. All are original creations, there was no use of templates that may need to credit the creators. The images we got from Canva are all copyright-free.</h1></div>
+
 
 
 
