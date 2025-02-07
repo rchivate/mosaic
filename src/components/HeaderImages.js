8333bee7adf186a3e704aad6e1676bfacb7ab9b1
healthy-restaurant <chivate.rsr@gmail.com>
2025-02-06 21:32:34 -0500

    Initial commit umgs

--- src/components/HeaderImages.js
+++ src/components/HeaderImages.js
@@ -30,38 +30,39 @@ export default function HeaderImages() {
             >
                 <div>
                     <h1 className='carousel-h1'>Algeria</h1>
-                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/algeria-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/algeria-flag.png' alt="Algeria" />
 
                     <p className='carousel-p'>A flavorful blend of Berber, French, Arab, and Mediterranean cuisine creates a melting pot of flavors in each dish.</p>
                 </div>
 
+
                 <div>
                     <h1 className='carousel-h1'>Ethiopia</h1>
-                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/ethiopia-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/ethiopia-flag.png' alt="Ethiopia" />
                     <p className='carousel-p'>A combination of spiced stews and vegetables served on top of Injera (Ethiopian flatbread) as a plate makes eating Ethiopian food a very unique and fun experience.</p>
                 </div>
 
                 <div>
                     <h1 className='carousel-h1'>France</h1>
-                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/france-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/france-flag.png' alt="France" />
                     <p className='carousel-p'>A combination of artistry and technique turn the simplest ingredients into luxurious meals.</p>
                 </div>
 
                 <div>
                     <h1 className='carousel-h1'>Greece</h1>
-                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/greece-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/greece-flag.png' alt="Greece" />
                     <p className='carousel-p'>A combination of fresh simple ingredients like olive oil, herbs, yogurt, and vegetables come together to create fresh, light, delicious, and healthy vegetarian meals.</p>
                 </div>
 
                 <div>
                     <h1 className='carousel-h1'>India</h1>
-                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/india-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='https://healthyresta.github.io/dinner/images/india-flag.png' alt="India" />
                     <p className='carousel-p'>A combination of vegetables, spices, roti (Indian flatbread), and rice make very delicious, flavorful vegetarian dishes.</p>
                 </div>
 
                 <div>
                     <h1 className='carousel-h1'>Mexico</h1>
-                    <img className='carousel-image' src='images/mexico-flag.png' alt="Placeholder 1" />
+                    <img className='carousel-image' src='images/mexico-flag.png' alt="Mexico" />
                     <p className='carousel-p'>A combination of Indigenous and Spanish cuisine creates bold, vibrant, spicy flavors in each dish.</p>
                 </div>
 
